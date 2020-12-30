c1a8d133801958542a8e1f7ea33fd30009c282c2
Will Kencel <wkencel1@gmail.com>
2020-12-30 13:42:58 -0500

    context provider description

--- client/components/ContextProvider.js
+++ client/components/ContextProvider.js
@@ -1,4 +1,8 @@
 import React from "react";
+
+// this allows us to change state without prop drilling
+// here is a deeper read on react context provider
+// https://medium.com/@willhowardgb/reacts-context-api-in-5-minutes-8188d9b507fe
 export const AppContext = React.createContext({
   user: {},
   currentSchool_id: "1",
@@ -9,3 +13,5 @@ export const AppContext = React.createContext({
   setCurrentSchool_id: () => {},
   setCurrentEvent_id: () => {},
 });
+
+// this is imported into App.jsx
